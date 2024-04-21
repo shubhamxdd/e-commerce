db157aedb70d8ae4983454b149562371d2a1dbf0
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-04-21 15:54:34 +0530

    increase skeleton width

--- app/(root)/_components/ProductsPageSkeleton.tsx
+++ app/(root)/_components/ProductsPageSkeleton.tsx
@@ -5,7 +5,7 @@ const ProductsPageSkeleton = () => {
   return (
     <>
       <div className="my-4 ">
-        <Skeleton className="h-10 w-24" />
+        <Skeleton className="h-10 w-60" />
       </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 place-items-center my-4">
         <ProductCardSkeleton />
