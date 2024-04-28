82c4d265a4ce72145c8ae24f8864ffe63c1b8d80
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-04-28 17:49:40 +0530

    Create register modal and register users

---
+++ app/(root)/_components/RegisterForm.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
+import { cn } from "@/lib/utils";
+import { FormEvent, useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { signIn } from "next-auth/react";
+
+export default function ProfileForm({
+  className,
+}: React.ComponentProps<"form">) {
+  const [data, setData] = useState({
+    email: "",
+    name: "",
+    password: "",
+    username: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const { toast } = useToast();
+
+  const onSubmit = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+
+      console.log(res);
+
+      toast({
+        title: "User created successfully",
+        description: "You will be signed in shortly !",
+      });
+
+      await signIn("credentials", {
+        email: data.email,
+        password: data.password,
+      }).then(() => {
+        toast({
+          title: "User logged in successfully",
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Error creating user",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form
+      className={cn("grid items-start gap-4", className)}
+      onSubmit={onSubmit}
+    >
+      <div className="grid gap-2">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          disabled={loading}
+          type="email"
+          id="email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="name">Name</Label>
+        <Input
+          disabled={loading}
+          type="name"
+          id="name"
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="username">Username</Label>
+        <Input
+          disabled={loading}
+          type="username"
+          id="username"
+          value={data.username}
+          onChange={(e) => setData({ ...data, username: e.target.value })}
+        />
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          disabled={loading}
+          id="password"
+          type="password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+      </div>
+      <Button type="submit" disabled={loading}>
+        {loading ? (
+          <div className="animate-spin flex justify-center items-center">
+            <AiOutlineLoading3Quarters size={24} />
+          </div>
+        ) : (
+          "Register"
+        )}
+      </Button>
+    </form>
+  );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,28 +3,36 @@ import HomePageProducts from "./_components/HomePageProducts";
 import { getLatestProducts, getPopularProducts } from "./_actions/action";
 import { Suspense } from "react";
 import HomePageSkeleton from "./_components/HomePageSkeleton";
+import LoginModal from "./_components/LoginModal";
+import RegisterModal from "./_components/RegisterModal";
+// import { getServerSession } from "next-auth";
 
-const RootPage = () => {
+const RootPage = async () => {
+  // const session = await getServerSession();
   return (
-    <Suspense
-      fallback={
-        <>
-          <HomePageSkeleton />
-          <HomePageSkeleton />
-        </>
-      }
-    >
-      <div className="space-y-12">
-        <HomePageProducts
-          title="Most Popular 🔥"
-          fetchFn={getPopularProducts}
-        />
-        <HomePageProducts
-          title="Newest Arrivals 🆕"
-          fetchFn={getLatestProducts}
-        />
-      </div>
-    </Suspense>
+    <>
+      {/* <LoginModal /> */}
+      <RegisterModal />
+      <Suspense
+        fallback={
+          <>
+            <HomePageSkeleton />
+            <HomePageSkeleton />
+          </>
+        }
+      >
+        <div className="space-y-12">
+          <HomePageProducts
+            title="Most Popular 🔥"
+            fetchFn={getPopularProducts}
+          />
+          <HomePageProducts
+            title="Newest Arrivals 🆕"
+            fetchFn={getLatestProducts}
+          />
+        </div>
+      </Suspense>
+    </>
   );
 };
 
